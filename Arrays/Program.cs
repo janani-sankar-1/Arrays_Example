6ad316d377e823628fdc9e285d71729cdda024ce
Janani S <Janani S@DESKTOP-13MT9AS>
2024-10-15 11:01:35 +0530

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ArraysExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            //create an array
+            int[] a = new int[5] { 10, 20, 30, 40, 50 };
+            string[] b = new string[5] { "one", "two", "three", "four", "five" };
+
+            //display the values of elements
+            Console.WriteLine(a[0]);
+            Console.WriteLine(a[1]);
+            Console.WriteLine(a[2]);
+            Console.WriteLine(a[3]);
+            Console.WriteLine(a[4]);
+            Console.WriteLine();
+            Console.WriteLine(b[0]);
+            Console.WriteLine(b[1]);
+            Console.WriteLine(b[2]);
+            Console.WriteLine(b[3]);
+            Console.WriteLine(b[4]);
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < b.Length; i++)
+            {
+                Console.WriteLine(b[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
